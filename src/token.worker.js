46e16d16a14319759dc7812a16e6905f0239792c
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-02-16 14:37:40 +0100

    remove logs from worker

--- src/token.worker.js
+++ src/token.worker.js
@@ -27,9 +27,6 @@ self.addEventListener('activate', function (_event) {
 // }
 
 self.addEventListener('message', (event) => {
-  console.debug(event)
-  console.debug('wait to trigger refresh')
-
   let data = event.data
   let { refreshTrigger } = data
 
