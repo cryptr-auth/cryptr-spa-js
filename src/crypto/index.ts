94b7f4156624ed3986324351595d50bd006fd07c
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-03-24 14:35:13 +0100

    improve crypto rndom

--- src/crypto/index.ts
+++ src/crypto/index.ts
@@ -4,33 +4,22 @@ import { sha256 } from './sha256'
 import secureRandom from 'secure-random'
 import * as Sentry from '@sentry/browser'
 
-let getRandomBytes = (typeof window !== 'undefined' && window.crypto
-  ? function () {
-      // Browsers
-      var crypto = window.crypto,
-        QUOTA = 65536
-      return function (n: number) {
-        var a = new Uint8Array(n)
-        for (var i = 0; i < n; i += QUOTA) {
-          crypto.getRandomValues(a.subarray(i, i + Math.min(n - i, QUOTA)))
-        }
-        return a
-      }
-    }
-  : function () {
-      // Node
-      return require('crypto').randomBytes
-    })()
+const RANDOM_LENGTH = 43
 
 const Crypto = {
   random: (): string => {
     try {
-      const random = secureRandom(32)
+      const random = secureRandom(RANDOM_LENGTH)
       return btoa(random).substring(0, 128)
     } catch (error) {
       Sentry.captureException(error)
-      const random = getRandomBytes(32)
-      return btoa(random).substring(0, 128)
+      const randomBytes = window.crypto.getRandomValues(new Uint8Array(RANDOM_LENGTH));
+      // Make compliant the random for code_verifier of PKCE
+      // abnf_unreserved = ALPHA / DIGIT / "-" / "." / "_" / "~"
+      const ABNF = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
+      let cryptoRandom = '';
+      randomBytes.forEach(v => (cryptoRandom += ABNF[v % ABNF.length]));
+      return cryptoRandom
     }
   },
   sha256: (message: string): string => sha256(message, 'base64') || '',
