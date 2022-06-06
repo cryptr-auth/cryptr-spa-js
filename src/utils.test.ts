9c676500caaa5590b0572cb6208681c72448e6b1
Thibaud RENAUX <thibaud.renaux@gmail.com>
2022-06-06 09:08:54 +0200

    add missing files for previous commit

---
+++ src/utils.test.ts
@@ -0,0 +1,62 @@
+import { parseRedirectParams } from './utils'
+
+describe('parseRedirectParams', () => {
+  it('should throw error if no location', () => {
+    expect(() => {
+      parseRedirectParams()
+    }).toThrowError('Can not parse authorization params')
+  })
+
+  it('should throw error if only state', () => {
+    expect(() => {
+      parseRedirectParams('?state=12')
+    }).toThrowError('Can not parse authorization params')
+  })
+
+  it('should throw error if only authorization_id', () => {
+    expect(() => {
+      parseRedirectParams('?authorization_id=12')
+    }).toThrowError('Can not parse authorization params')
+  })
+
+  it('should throw error if only code', () => {
+    expect(() => {
+      parseRedirectParams('?code=12')
+    }).toThrowError('Can not parse authorization params')
+  })
+
+  it('should return standard redirect params if code id and state params present', () => {
+    expect(parseRedirectParams('?state=42&authorization_id=azerty&code=12')).toEqual({
+      state: '42',
+      authorization: {
+        code: '12',
+        id: 'azerty',
+      },
+    })
+  })
+
+  it('should return redirect params with organization_domain if present', () => {
+    expect(
+      parseRedirectParams('?state=42&authorization_id=azerty&code=12&organization_domain=misapret'),
+    ).toEqual({
+      organization_domain: 'misapret',
+      state: '42',
+      authorization: {
+        code: '12',
+        id: 'azerty',
+      },
+    })
+  })
+
+  it('should return standard redirect params if organization_domain is blank', () => {
+    expect(
+      parseRedirectParams('?state=42&authorization_id=azerty&code=12&organization_domain='),
+    ).toEqual({
+      state: '42',
+      authorization: {
+        code: '12',
+        id: 'azerty',
+      },
+    })
+  })
+})
