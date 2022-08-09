669a69de74fbcb371f54a50e7c34c2e3bdfe0490
Thibaud RENAUX <thibaud.renaux@gmail.com>
2022-08-09 18:56:43 +0200

    update PKCe to use crypto-js

--- src/pkce.ts
+++ src/pkce.ts
@@ -1,17 +1,24 @@
 import Crypto from './crypto'
+import CryptoJS  from 'crypto-js'
 
 import { v4 as uuid } from 'uuid'
 
 import { ProofKeyChallengeExchange } from './interfaces'
 import { SHA256 } from './constants'
 
+const base64UrlEncode = (str: string) => {
+  return str.replace(/\+/g, '-').replace(/\//g, '_').replace(/[=]/g, '');
+}
+
 const codeChallenge = (codeVerifier: string): string => {
-  return Crypto.sha256(codeVerifier)
+  // return Crypto.sha256(codeVerifier)
+  return base64UrlEncode(CryptoJS.SHA256(codeVerifier).toString(CryptoJS.enc.Base64))
 }
 
 const Pkce = {
   gen: (state = uuid()): ProofKeyChallengeExchange => {
-    const codeVerifier = Crypto.random()
+    // const codeVerifier = Crypto.random()
+    const codeVerifier = base64UrlEncode(uuid())
     return {
       code_verifier: codeVerifier,
       code_challenge: codeChallenge(codeVerifier),
