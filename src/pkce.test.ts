4e62e2fb08d15547853d43de05cc02a9b933405c
Thibaud RENAUX <thibaud.renaux@gmail.com>
2022-08-10 16:38:14 +0200

    handle fixed_pkce for pkce generation

--- src/pkce.test.ts
+++ src/pkce.test.ts
@@ -2,8 +2,8 @@ import Pkce from './pkce'
 import Crypto from './crypto/index'
 
 describe('Pkce', () => {
-  it('gen() returns a challenge with its code verifier', async () => {
-    expect(Pkce.gen()).toMatchObject({
+  it('gen(false) returns a challenge with its code verifier', async () => {
+    expect(Pkce.gen(false)).toMatchObject({
       code_verifier: expect.any(String),
       code_challenge: expect.any(String),
       code_challenge_method: 'S256',
@@ -11,8 +11,8 @@ describe('Pkce', () => {
     })
   })
 
-  it('gen() returns Base64 URLcoded challenge and code verifier', async () => {
-    let pkce = Pkce.gen()
+  it('gen(false) returns Base64 URL encoded challenge and code verifier', async () => {
+    let pkce = Pkce.gen(true)
     expect(pkce).toMatchObject({
       code_verifier: expect.any(String),
       code_challenge: expect.any(String),
@@ -23,9 +23,34 @@ describe('Pkce', () => {
     expect(pkce.code_challenge).toEqual(Crypto.sha256Base64UrlEncoded(pkce.code_verifier))
   })
 
-  it('gen(uuid) returns a Pkce with specified uuid state', async () => {
+  it('gen(false) returns Base64 challenge and code verifier', async () => {
+    let pkce = Pkce.gen(false)
+    expect(pkce).toMatchObject({
+      code_verifier: expect.any(String),
+      code_challenge: expect.any(String),
+      code_challenge_method: 'S256',
+      state: expect.any(String),
+    })
+    expect(pkce.code_verifier.length).toEqual(128)
+    expect(pkce.code_challenge).toEqual(Crypto.sha256(pkce.code_verifier))
+  })
+
+  it('gen(false, uuid) returns a Pkce with specified uuid state', async () => {
     const state = 'dc9fc366-3b36-4465-b547-e43b45d34076'
-    let pkceState = Pkce.gen(state)
+    let pkceState = Pkce.gen(false, state)
+    expect(pkceState).toMatchObject({
+      code_verifier: expect.any(String),
+      code_challenge: expect.any(String),
+      code_challenge_method: 'S256',
+      state: 'dc9fc366-3b36-4465-b547-e43b45d34076',
+    })
+    expect(pkceState.code_verifier.length).toEqual(128)
+    expect(pkceState.code_challenge).toEqual(Crypto.sha256(pkceState.code_verifier))
+  })
+
+  it('gen(true uuid) returns a Pkce with specified uuid state and B64 Url encoded values', async () => {
+    const state = 'dc9fc366-3b36-4465-b547-e43b45d34076'
+    let pkceState = Pkce.gen(true, state)
     expect(pkceState).toMatchObject({
       code_verifier: expect.any(String),
       code_challenge: expect.any(String),
--- src/pkce.ts
+++ src/pkce.ts
@@ -6,15 +6,22 @@ import { ProofKeyChallengeExchange } from './interfaces'
 import { SHA256 } from './constants'
 
 const Pkce = {
-  gen: (state = uuid()): ProofKeyChallengeExchange => {
-    const codeVerifier = Crypto.randomB64UrlEncoded()
+  gen: (fixedPkce: boolean, state = uuid()): ProofKeyChallengeExchange => {
+    const codeVerifier = Pkce.genCodeVerifier(fixedPkce)
+    const codeChallenge = Pkce.genCodeChallenge(fixedPkce, codeVerifier)
 
     return {
       code_verifier: codeVerifier,
-      code_challenge: Crypto.sha256Base64UrlEncoded(codeVerifier),
+      code_challenge: codeChallenge,
       code_challenge_method: SHA256,
       state: state,
     }
+  },
+  genCodeVerifier: (fixedPkce: boolean): string => {
+    return fixedPkce ? Crypto.randomB64UrlEncoded() : Crypto.random()
+  },
+  genCodeChallenge: (fixedPkce: boolean, codeVerifier: string): string => {
+    return fixedPkce ? Crypto.sha256Base64UrlEncoded(codeVerifier) : Crypto.sha256(codeVerifier)
   },
 }
 
